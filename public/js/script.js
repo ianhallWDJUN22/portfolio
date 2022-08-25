972b397076ba584ca9001d559fa18ae50db0732a
Ian Hall <ianmhall6@gmail.com>
2022-08-25 13:08:09 -0400

    first commit for portfolio site

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("portfolio JS imported successfully!");
+  },
+  false
+);
---
+++ routes/index.routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+module.exports = router;
+
+router.get('/project1', (req, res, next) => {
+  res.render('projects/project1.hbs');
+});
+
+router.get('/project2', (req, res, next) => {
+  res.render('projects/project2.hbs');
+});
+
+router.get('/project3', (req, res, next) => {
+  res.render('projects/project3.hbs');
+});
+
+
